916566dac70a3c19d1a627b560533569a454cc5b
nguyenchanhthong2102 <124759370+nguyenchanhthong2102@users.noreply.github.com>
2023-02-08 13:03:10 +0700

    dev upload
    upload code

---
+++ main.js
@@ -0,0 +1,6 @@
+const menu=document.querySelector('.fa-bars');
+menu.addEventListener('click',function(){
+    let nav_bar=document.querySelector('.nav_button');
+    nav_bar.classList.toggle('active');
+})
+
